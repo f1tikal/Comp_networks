0d8ab6dbfa17d6aa7115a5f8ff0049dbe3a63520
f1tikal <103326959+f1tikal@users.noreply.github.com>
2025-12-19 21:56:11 +0300

    Add files via upload

---
+++ Server_TCP_h.cpp
@@ -0,0 +1,60 @@
+﻿// SERVER TCP 
+#include <iostream>  
+#include <winsock2.h> 
+#include <windows.h> 
+#include <string> 
+#include <sstream>
+#pragma comment (lib, "Ws2_32.lib")  
+using namespace std;
+#define SRV_PORT 1234  
+#define BUF_SIZE 64  
+const string QUEST = "TELL IMT: ves/rost\n";
+int main() {
+	char buff[1024];
+	if (WSAStartup(0x0202, (WSADATA*)&buff[0]))
+	{
+		cout << "Error WSAStartup \n" << WSAGetLastError();   // Ошибка!
+		return -1;
+	}
+	SOCKET s, s_new;
+	int from_len;
+	char buf[BUF_SIZE] = { 0 };
+	sockaddr_in sin, from_sin;
+	s = socket(AF_INET, SOCK_STREAM, 0);
+	sin.sin_family = AF_INET;
+	sin.sin_addr.s_addr = 0;
+	sin.sin_port = htons(SRV_PORT);
+	bind(s, (sockaddr*)&sin, sizeof(sin));
+	string msg, msg1;
+	listen(s, 3);
+	while (1) {
+		from_len = sizeof(from_sin);
+		s_new = accept(s, (sockaddr*)&from_sin, &from_len);
+		cout << "new connected client! " << endl;
+		msg = QUEST;
+		while (1) {
+			send(s_new, (char*)&msg[0], msg.size(), 0);
+			from_len = recv(s_new, (char*)buf, BUF_SIZE, 0);
+			buf[from_len] = 0;
+			msg1 = (string)buf;
+			cout << msg1 << endl;;
+			if (msg1 == "Bye") break;
+			stringstream ss(msg1);
+			int first, second;
+			ss >> first >> second;
+			cout << first / pow(static_cast<double>(second) / 100, 2)<<"\n";
+			if (first / pow(static_cast<double>(second)/100, 2) >=25) {
+				msg = "FAT";
+			}
+			else if (first / pow(static_cast<double>(second) / 100, 2) <= 19) {
+				msg = "SKINNY";
+			}
+			else {
+				msg = "NORMAL";
+			}
+		}
+		cout << "client is lost";
+		closesocket(s_new);
+	}
+	return 0;
+}
